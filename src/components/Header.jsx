9449a39ef4749188b8736d6fa689df07850fa791
yaqzaan <97394346+yaqzaan@users.noreply.github.com>
2023-03-19 18:04:06 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+    return ( 
+        <div>
+          <h1>Expense Tracker</h1>  
+        </div>
+     );
+}
+ 
+export default Header;
